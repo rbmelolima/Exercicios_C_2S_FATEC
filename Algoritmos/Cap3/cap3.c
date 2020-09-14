a4f5315ee0eace39bebdbe8bc7bae5480efd58ff
Jean Skurtinski <jean.jose@kbrtec.com.br>
2020-09-14 15:53:27 -0300

    Cap3 ex2 e ex5

--- Algoritmos/Cap3/cap3.c
+++ Algoritmos/Cap3/cap3.c
@@ -3,6 +3,38 @@
 
 #define ex4
 
+#ifdef ex2
+main()
+{
+    int valores[10], aux, i, j, k;
+    char tecla;
+    do
+    {
+        for(i = 0; i < 10; i++){
+            printf("\nDigite o %d inteiro: ", i+1);
+            scanf("%d",&valores[i]);
+            if(i != 0){
+	            k = i;
+	            for(j = i-1; j >= 0; j--){
+	                if(valores[j] > valores[k]){
+	                    aux = valores[j];
+	                    valores[j] = valores[k];
+	                    valores[k] = aux;
+	                }
+	                k = j;
+	            }
+			}
+        }
+        for(i = 0; i < 10; i++){
+            printf("\no %d inteiro do vetor e %d ", i+1, valores[i]);
+        }
+        printf("\nDeseja continuar?(s/n) ");
+        getchar();
+        tecla = getchar();
+    } while ((tecla != 'n') && (tecla != 'N'));
+}
+#endif // ex2
+
 #ifdef ex3
 int main() {
     int lengthComponents = 10;
@@ -63,3 +95,41 @@ int main() {
 
 }
 #endif // ex4
+
+#ifdef ex5
+main()
+{
+    float matriz[2][2], x;
+    int i, j;
+    char tecla;
+    int encontrado = 0;
+    do
+    {
+    	encontrado = 0;
+        for(i = 0; i < 2; i++){
+            for(j = 0; j < 2; j++){
+                printf("\nDigite o valor da posicao [%d][%d] da matriz: ", i, j);
+                scanf("%f", &matriz[i][j]);
+            }
+        }
+        printf("\nDigite o valor que deseja buscar na matriz: ");
+        scanf("%f", &x);
+        i = 0;
+        j = 0;
+        for(i = 0; i < 2; i++){
+            for(j = 0; j < 2; j++){
+                if(x == matriz[i][j]){
+                    encontrado = 1;
+                    printf("\nO valor %f esta presente na posicao [%d][%d] ", x, i, j);
+                }
+            }
+        }
+        if(encontrado == 0){
+            printf("\nO valor %f nao foi encontrado na matriz!", x);
+        }
+        printf("\n\nDeseja continuar?(s/n) ");
+        getchar();
+        tecla = getchar();
+    } while ((tecla != 'n') && (tecla != 'N'));
+}
+#endif // ex5
