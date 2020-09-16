d59c62cf677fb5b36b01302f5c345af8d45fda27
Jean Skurtinski <jean.jose@kbrtec.com.br>
2020-09-15 21:04:15 -0300

    Cap4 ex3 e ex4

---
+++ Algoritmos/Cap4/cap4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define ex3
+
+#ifdef ex3
+main()
+{
+    char caracteres[10];
+    char tecla;
+    int i;
+    do
+    {
+        printf("Digite uma string via teclado : ");
+		gets(caracteres);
+		for(i = 0; i < 10; i++){
+			if((int)caracteres[i] >= 97 && (int)caracteres[i] <= 122){
+				caracteres[i] = ((int)caracteres[i])-32;
+			}
+		}
+		printf("A string digitada com seus caracteres maiusculus e \"%s\"", caracteres);
+        printf("\nDeseja continuar?(s/n) ");
+        tecla = getchar();
+        getchar();
+    } while ((tecla != 'n') && (tecla != 'N'));
+}
+#endif
+
+#ifdef ex4
+main()
+{
+    char caracteres[10];
+    char tecla;
+    int i;
+    do
+    {
+        printf("Digite uma string via teclado : ");
+		gets(caracteres);
+		for(i = 0; i < 10; i++){
+			if((int)caracteres[i] >= 65 && (int)caracteres[i] <= 90){
+				caracteres[i] = ((int)caracteres[i])-32;
+			}
+		}
+		printf("A string digitada com seus caracteres minusculos e \"%s\"", caracteres);
+        printf("\nDeseja continuar?(s/n) ");
+        tecla = getchar();
+        getchar();
+    } while ((tecla != 'n') && (tecla != 'N'));
+}
+#endif
