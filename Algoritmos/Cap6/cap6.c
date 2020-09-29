6342becb603f2e360729c9ca263dbbf5ea31a6d9
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-09-29 19:29:20 -0300

    feat(cap6): iniciando o cap6

---
+++ Algoritmos/Cap6/cap6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define ex1
+
+#ifdef ex1
+void main()
+{
+}
+#endif
+
+#ifdef ex2
+void main()
+{
+}
+#endif
+
+#ifdef ex3
+void main()
+{
+}
+#endif
+
+#ifdef ex4
+void main()
+{
+}
+#endif
+
+#ifdef ex5
+void main()
+{
+}
+#endif
