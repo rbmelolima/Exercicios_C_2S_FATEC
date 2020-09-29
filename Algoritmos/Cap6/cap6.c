5fff565333b4346ea3158a2e2d019b1d64847967
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-09-29 19:35:18 -0300

    feat(cap6): implementando rotina em todos os exercícios

--- Algoritmos/Cap6/cap6.c
+++ Algoritmos/Cap6/cap6.c
@@ -6,29 +6,64 @@
 #ifdef ex1
 void main()
 {
+  int running = 1;
+
+  while(running == 1){
+    printf("\n\nDeseja continuar? (1) Sim (0) Não: ");
+    scanf("%d", &running);
+    system("cls");
+  }
 }
 #endif
 
 #ifdef ex2
 void main()
 {
+  int running = 1;
+
+  while(running == 1){
+    printf("\n\nDeseja continuar? (1) Sim (0) Não: ");
+    scanf("%d", &running);
+    system("cls");
+  }
 }
 #endif
 
 #ifdef ex3
 void main()
 {
+  int running = 1;
+
+  while(running == 1){
+    printf("\n\nDeseja continuar? (1) Sim (0) Não: ");
+    scanf("%d", &running);
+    system("cls");
+  }
 }
 #endif
 
 #ifdef ex4
 void main()
 {
+  int running = 1;
+
+  while(running == 1){
+    printf("\n\nDeseja continuar? (1) Sim (0) Não: ");
+    scanf("%d", &running);
+    system("cls");
+  }
 }
 #endif
 
 #ifdef ex5
 void main()
 {
+  int running = 1;
+
+  while(running == 1){
+    printf("\n\nDeseja continuar? (1) Sim (0) Não: ");
+    scanf("%d", &running);
+    system("cls");
+  }
 }
 #endif
