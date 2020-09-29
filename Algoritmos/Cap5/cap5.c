8794bb16aaa43ef12d6f726c23f86b29a76ec055
Jean Skurtinski <jean.jose@kbrtec.com.br>
2020-09-29 10:57:28 -0300

    Cap5 ex 4 e ex 5

--- Algoritmos/Cap5/cap5.c
+++ Algoritmos/Cap5/cap5.c
@@ -43,3 +43,50 @@ main()
 
 }
 #endif
+
+
+#ifdef ex4
+main()
+{
+    int num, div, mult, continuar; 
+    do{
+        printf("Digite um numero inteiro: ");
+        scanf("%i", &num);
+        div = (num >> 3);
+        mult = (num << 3);
+        printf("\nO numero digitado foi %d, ele dividido por 8 e' %d e multiplicado por 8 e' %d", num, div, mult);
+        printf("\nDigite (1) para continuar ou (0) para sair: ");
+        scanf("%i", &continuar);
+    }while(continuar == 1);
+
+}
+#endif
+
+#ifdef ex5
+main()
+{
+	char data_nasc[11];
+	char data_agora[11];
+	int dias_meses[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int dia, mes, ano, continuar, mes_tira, ano_tira, dias_mes; 
+    do{
+        printf("\nDigite a data de nascimento (dd/mm/yyyy): ");
+        scanf("%s", &data_nasc);
+        printf("\nDigite a data de calculo (dd/mm/yyyy): ");
+        scanf("%s", &data_agora);
+        ano = ( (data_agora[6] * 1000) + (data_agora[7] * 100) + (data_agora[8] * 10) + data_agora[9] ) - ((data_nasc[6] * 1000) + (data_nasc[7] * 100) + (data_nasc[8] * 10) + data_nasc[9]);
+        ano_tira = ( ( (data_agora[3] * 10) + data_agora[4]) < ( (data_nasc[3] * 10) + data_nasc[4]) );
+        mes = (ano_tira == 1)? ( ( (data_agora[3] * 10) + data_agora[4] + 12) - ( (data_nasc[3] * 10) + data_nasc[4]) ) : ( ( (data_agora[3] * 10) + data_agora[4]) - ( (data_nasc[3] * 10) + data_nasc[4]) ) ;
+        mes_tira = ( ( (data_agora[0] * 10) + data_agora[1]) < ( (data_nasc[0] * 10) + data_nasc[1]) );
+        dia = (mes_tira == 1)? ( ( (data_agora[0] * 10) + data_agora[1] + 30) - ( (data_nasc[0] * 10) + data_nasc[1]) ) : ( ( (data_agora[0] * 10) + data_agora[1]) - ( (data_nasc[0] * 10) + data_nasc[1]) ) ;
+        mes = mes - mes_tira;
+        ano = ano - ano_tira;
+        printf("%d %d %d", dia, mes, ano);
+        
+        
+        printf("\nDigite (1) para continuar ou (0) para sair: ");
+        scanf("%i", &continuar);
+    }while(continuar == 1);
+
+}
+#endif
