f5510f883f4f1ee5413cc6fd2c36993f45829e64
Jean Skurtinski <jean.jose@kbrtec.com.br>
2020-11-03 20:36:38 -0300

    Ex3

---
+++ Algoritmos/Cap7/cap7.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ex3
+
+#ifdef ex1
+void main()
+{
+  int running = 1;
+
+  while(running == 1){
+    printf("\n\nDeseja continuar? (1) Sim (0) Não: ");
+    scanf("%d", &running);
+    system("cls");
+  }
+}
+#endif
+
+#ifdef ex2
+void main()
+{
+  int running = 1;
+
+  while(running == 1){
+    printf("\n\nDeseja continuar? (1) Sim (0) Não: ");
+    scanf("%d", &running);
+    system("cls");
+  }
+}
+#endif
+
+#ifdef ex3
+    struct ponto{
+        float D;
+        float X;
+        float Y;
+    };
+
+    float calculaDistancia(struct ponto p){
+        float d;
+        d = sqrt( pow(p.X, 2) + pow(p.Y, 2) );
+        return(d);
+    }
+
+    void main()
+    {
+        int running = 1;
+        struct ponto pont;
+        while(running == 1){
+            printf("Digite a coordenada X do ponto: \n");
+            scanf("%f",&pont.X);
+            system("cls");
+            printf("Digite a coordenada Y do ponto: \n");
+            scanf("%f",&pont.Y);
+            system("cls");
+            pont.D = calculaDistancia(pont);
+            printf("\nA distancia do ponto (%f, %f) ate a origem e %f \n", pont.X, pont.Y, pont.D );
+            printf("\n\nDeseja continuar? (1) Sim (0) Nao: ");
+            scanf("%d", &running);
+            system("cls");
+        }
+    }
+#endif
